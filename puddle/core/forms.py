7bd3641a525f81d94ffb613757bcb219f678bc9b
PMagoga <pmagoga@gmail.com>
2023-12-10 18:20:29 -0300

    atualizando

--- puddle/core/forms.py
+++ puddle/core/forms.py
@@ -7,3 +7,20 @@ class SignupForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2')
+        
+    username = forms.CharField(widget=forms.TextInput(attrs={
+        'placeholder': "Seu username",
+        'class': 'w-full py-4 px-6 rounded-xl'
+    }))
+    email = forms.CharField(widget=forms.EmailInput(attrs={
+        'placeholder': "Seu Endereço de e-mail",
+        'class': 'w-full py-4 px-6 rounded-xl'
+    }))
+    password1 = forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder': "Sua senha",
+        'class': 'w-full py-4 px-6 rounded-xl'
+    }))
+    password2 = forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder': "Repita sua senha",
+        'class': 'w-full py-4 px-6 rounded-xl'
+    }))
